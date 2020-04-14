e30402e59973b86b37371cc3ac9089833c3da149
siplay <siplay92@gmail.com>
2020-04-14 20:32:43 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-host-docker
+
+nginx + php-fpm 7.2 + memcached + mysql + postgres + rabbitmq + tarantool
---
+++ images/mysql/create_second_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS db_bitrix;
+GRANT ALL PRIVILEGES ON db_bitrix.* TO 'default';
+CREATE DATABASE IF NOT EXISTS db_smf;
+GRANT ALL PRIVILEGES ON db_smf.* TO 'default';
+CREATE DATABASE IF NOT EXISTS db_symfony;
+GRANT ALL PRIVILEGES ON db_symfony.* TO 'default';
+CREATE DATABASE IF NOT EXISTS db_socialsgrowth;
+GRANT ALL PRIVILEGES ON db_socialsgrowth.* TO 'default';
